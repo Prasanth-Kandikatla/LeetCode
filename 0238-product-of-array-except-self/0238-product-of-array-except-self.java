a5ff722e23115f2e9ef739b6201dcdc7b938d885
Prasanth Kandikatla <008480414@coyote.csusb.edu>
2025-10-15 13:37:18 -0700

    Time: 2 ms (87.58%), Space: 55.4 MB (76.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -14,17 +14,53 @@ class Solution {
         //     }
         //    return arr;
 
-        int n = nums.length;
-        int[] result = new int[n];
-        result[0] = 1;
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
-        }
+        //O(N) Approach 2
+        // int n = nums.length;
+        // int[] result = new int[n];
+        // result[0] = 1;
+        // for (int i = 1; i < n; i++) {
+        //     result[i] = result[i - 1] * nums[i - 1];
+        // }
+        // int product = 1;
+        // for (int i = n - 1; i >= 0; i--) {
+        //     result[i] *= product;
+        //     product *= nums[i];
+        // }
+        // return result;
+
+        int count = 0;
         int product = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= product;
-            product *= nums[i];
+        for (int num: nums) {
+            if(num == 0) {
+                count++;
+                continue;
+            }
+            product *= num;
         }
-        return result;
+
+        int[] arr = new int[nums.length];
+        if(count > 1) {
+            
+            for(int i = 0; i<nums.length; i++) {
+                arr[i] = 0;
+            }
+        }
+
+        else if(count == 1) {
+            for(int i = 0; i<nums.length; i++) {
+                if(nums[i] == 0) {
+                    arr[i] = product;
+                    continue;
+                }
+                arr[i] = 0;
+            }
+        } else {
+            for(int i = 0; i<nums.length; i++) {
+                arr[i] = product/nums[i];
+            }
+        }
+        return arr;
+
+
     }
 }
