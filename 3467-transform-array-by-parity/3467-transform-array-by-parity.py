2a3a17e358cb291d013aba764ca25e99d5ba8cae
Prasanth Kandikatla <008480414@coyote.csusb.edu>
2026-05-01 16:13:54 -0700

    Time: 3 ms (27.96%), Space: 19.4 MB (19.06%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.py
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.py
@@ -5,13 +5,8 @@ class Solution:
         result = []
         for i in nums:
             if i%2 == 0:
-                even += 1
+                result.append(0)
             else:
-                odd += 1
-        while even > 0:
-            result.append(0)
-            even -= 1
-        while odd > 0:
-            result.append(1)
-            odd -= 1
+                result.append(1)
+        result.sort()
         return result
